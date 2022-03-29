326505d9840d4f7fcac8bf983fa9eae2d247ef79
Oscar Dowson <odow@users.noreply.github.com>
2022-03-29 16:49:55 +1300

    Fix bugs and test examples/lp_test.jl (#3)

--- examples/lp_test.jl
+++ examples/lp_test.jl
@@ -1,45 +1,38 @@
 using UnitJuMP
-using GLPK
+using Test
+import HiGHS
 
-m = Model(GLPK.Optimizer)
+function test_simple_example()
+    model = Model(HiGHS.Optimizer)
+    max_speed = 2u"m/s"
+    a = 9.8u"ft/s^2"
+    M = 0.5u"m/s"
+    no_speed = 0.0u"m/s"
+    data = 4
+    @variables(model, begin
+        x >= 0, u"m/s"
+        y >= 0, u"s"
+        z >= 0, u"km/s"
+        v[1:4], u"m/s"
+        w, Bin
+    end)
+    @constraints(model, begin
+        x + a * y <= max_speed
+        z + x <= max_speed
+        x + 0.5 * a * y <= max_speed
+        x <= M * w
+        -3x + 2z <= no_speed
+        x == 0.15 * max_speed
+        con7[i = 1:4], v[i] <= max_speed
+        con8, sum(v[i] for i in 1:3) <= max_speed, u"ft/s"
+        -3 * data * x + 2z <= no_speed
+    end)
+    @objective(model, Max, x + 0.2 * z)
+    optimize!(model)
+    @test value(x) == 0.3u"m/s"
+    @test value(y) == 0.0u"s"
+    @test value(z) ≈ 0.00045u"km/s"
+    return
+end
 
-const speedunit = u"m/s"
-const timeunit = u"s"
-
-@variable(m, x >= 0, speedunit)
-@variable(m, y >= 0, timeunit)
-@variable(m, z >= 0, u"km/s")
-
-@variable(m, v[1:4], speedunit)
-
-@variable(m, w, Bin)
-
-max_speed = 2u"m/s"
-a = 9.8u"ft/s^2"
-
-con = @constraint(m, x + a * y <= max_speed)
-@constraint(m, z + x <= max_speed)
-@constraint(m, x + 0.5 * a * y <= max_speed)
-
-M = 0.5u"m/s"
-@constraint(m, x <= M * w)
-
-no_speed = 0.0u"m/s"
-@constraint(m, -3x + 2z <= no_speed)
-
-@constraint(m, x == 0.15 * max_speed)
-
-@constraint(m, con7[i = 1:4], v[i] <= max_speed)
-
-@constraint(m, con8, sum(v[i] for i in 1:3) <= max_speed, u"ft/s")
-
-@objective(m, Max, x + 0.2 * z)
-
-data = 4
-@constraint(m, -3 * data * x + 2z <= no_speed)
-
-optimize!(m)
-
-value(m[:x])
-value(m[:y])
-value(m[:z])
+test_simple_example()
