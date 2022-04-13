b251e6887ea03c6f7c1fa6613d425b355cbab5b5
Truls Flatberg <Truls.Flatberg@sintef.no>
2022-04-13 16:14:01 +0200

    Implement VariableRef funcs for UnitVariableRef

--- src/units.jl
+++ src/units.jl
@@ -44,6 +44,102 @@ function JuMP.value(x::UnitVariableRef)
     return Unitful.Quantity(JuMP.value(x.variable), x.unit)
 end
 
+JuMP.name(x::UnitVariableRef) = JuMP.name(x.variable)
+
+function JuMP.set_name(x::UnitVariableRef, s::String)
+    return JuMP.set_name(x.variable, s)
+end
+
+function JuMP.set_start_value(x::UnitVariableRef, value::Unitful.Quantity)
+    val = Unitful.uconvert(x.unit, value)
+    return JuMP.set_start_value(x.variable, Unitful.ustrip(val))
+end
+
+function JuMP.set_start_value(x::UnitVariableRef, value::Real)
+    return error("Start value for unit variable must be given with units")
+end
+
+function JuMP.start_value(x::UnitVariableRef)
+    return Unitful.Quantity(JuMP.start_value(x.variable), x.unit)
+end
+
+JuMP.has_lower_bound(x::UnitVariableRef) = JuMP.has_lower_bound(x.variable)
+
+function JuMP.lower_bound(x::UnitVariableRef)
+    return Unitful.Quantity(JuMP.lower_bound(x.variable), x.unit)
+end
+
+function JuMP.set_lower_bound(x::UnitVariableRef, lower::Unitful.Quantity)
+    val = Unitful.uconvert(x.unit, lower)
+    return JuMP.set_lower_bound(x.variable, Unitful.ustrip(val))
+end
+
+function JuMP.set_lower_bound(x::UnitVariableRef, lower::Real)
+    return error("Lower bound of unit variable must be given with units")
+end
+
+function JuMP.delete_lower_bound(x::UnitVariableRef)
+    return JuMP.delete_lower_bound(x.variable)
+end
+
+JuMP.LowerBoundRef(x::UnitVariableRef) = JuMP.LowerBoundRef(x.variable)
+
+JuMP.has_upper_bound(x::UnitVariableRef) = JuMP.has_upper_bound(x.variable)
+
+function JuMP.upper_bound(x::UnitVariableRef)
+    return Unitful.Quantity(JuMP.upper_bound(x.variable), x.unit)
+end
+
+function JuMP.set_upper_bound(x::UnitVariableRef, upper::Unitful.Quantity)
+    val = Unitful.uconvert(x.unit, upper)
+    return JuMP.set_upper_bound(x.variable, Unitful.ustrip(val))
+end
+
+function JuMP.set_upper_bound(x::UnitVariableRef, upper::Real)
+    return error("Upper bound of unit variable must be given with units")
+end
+
+function JuMP.delete_upper_bound(x::UnitVariableRef)
+    return JuMP.delete_upper_bound(x.variable)
+end
+
+JuMP.UpperBoundRef(x::UnitVariableRef) = JuMP.UpperBoundRef(x.variable)
+
+JuMP.is_fixed(x::UnitVariableRef) = JuMP.is_fixed(x.variable)
+
+function JuMP.fix_value(x::UnitVariableRef)
+    return Unitful.Quantity(JuMP.fix_value(x.variable), x.unit)
+end
+
+function JuMP.fix(x::UnitVariableRef, value::Unitful.Quantity)
+    val = Unitful.uconvert(x.unit, value)
+    return fix(x.variable, Unitful.ustrip(val))
+end
+
+function JuMP.fix(x::UnitVariableRef, value::Real)
+    return error("Fix value of unit value must be given with units")
+end
+
+JuMP.unfix(x::UnitVariableRef) = JuMP.unfix(x.variable)
+
+JuMP.FixRef(x::UnitVariableRef) = JuMP.FixRef(x.variable)
+
+JuMP.is_integer(x::UnitVariableRef) = JuMP.is_integer(x.variable)
+
+JuMP.set_integer(x::UnitVariableRef) = JuMP.set_integer(x.variable)
+
+JuMP.unset_integer(x::UnitVariableRef) = JuMP.unset_integer(x.variable)
+
+JuMP.IntegerRef(x::UnitVariableRef) = JuMP.IntegerRef(x.variable)
+
+JuMP.is_binary(x::UnitVariableRef) = JuMP.is_binary(x.variable)
+
+JuMP.set_binary(x::UnitVariableRef) = JuMP.set_binary(x.variable)
+
+JuMP.unset_binary(x::UnitVariableRef) = JuMP.unset_binary(x.variable)
+
+JuMP.BinaryRef(x::UnitVariableRef) = JuMP.BinaryRef(x.variable)
+
 ###
 ### UnitExpression
 ###
@@ -109,6 +205,17 @@ end
 
 function JuMP.check_belongs_to_model(c::_UnitConstraint, model::AbstractModel)
     return JuMP.check_belongs_to_model(c.constraint, model)
+end
+
+function JuMP.dual(c::UnitConstraintRef)
+    return Unitful.Quantity(JuMP.dual(c.constraint), Unitful.unit(1 / c.unit))
+end
+
+function JuMP.shadow_price(c::UnitConstraintRef)
+    return Unitful.Quantity(
+        JuMP.shadow_price(c.constraint),
+        Unitful.unit(1 / c.unit),
+    )
 end
 
 function JuMP.build_constraint(
